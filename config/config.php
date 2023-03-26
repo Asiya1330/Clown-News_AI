a8212c0bf23bf25143d7f123b8d851f12aba19b2
Asiya1330 <asiya.batool987@gmail.com>
2023-03-26 20:34:20 +0500

    Clown_NEWS

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//The main config file
+define('BASE_URL', 'http://localhost:8080/clown-news/');
+define('ENV', 'dev');
+define('DEFAULT_MODULE', 'welcome');
+define('DEFAULT_CONTROLLER', 'Welcome');
+define('DEFAULT_METHOD', 'index');
+define('MODULE_ASSETS_TRIGGER', '_module');
+define('API_KEY', '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');
